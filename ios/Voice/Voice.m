57eabb03ec9d2e1e05b8ead3c2c4f84aa5b355e1
Pablo Queme <pgqueme@gmail.com>
2018-02-12 15:29:21 -0600

    Adding category change from @mrwordev

--- ios/Voice/Voice.m
+++ ios/Voice/Voice.m
@@ -35,9 +35,30 @@
 
     self.speechRecognizer.delegate = self;
 
+    /*----------
     NSError* audioSessionError = nil;
     self.audioSession = [AVAudioSession sharedInstance];
 
+    if (audioSessionError != nil) {
+        [self sendResult:RCTMakeError([audioSessionError localizedDescription], nil, nil) :nil :nil :nil];
+        return;
+    }
+    ----------*/
+
+    NSError* audioSessionError = nil;
+    self.audioSession = [AVAudioSession sharedInstance];
+    [self.audioSession setCategory:AVAudioSessionCategoryRecord error:&audioSessionError];
+    
+    if (audioSessionError != nil) {
+        [self sendResult:RCTMakeError([audioSessionError localizedDescription], nil, nil) :nil :nil :nil];
+        return;
+    }
+    [self.audioSession setMode:AVAudioSessionModeMeasurement error:&audioSessionError];
+    if (audioSessionError != nil) {
+        [self sendResult:RCTMakeError([audioSessionError localizedDescription], nil, nil) :nil :nil :nil];
+        return;
+    }
+    [self.audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:&audioSessionError];
     if (audioSessionError != nil) {
         [self sendResult:RCTMakeError([audioSessionError localizedDescription], nil, nil) :nil :nil :nil];
         return;
